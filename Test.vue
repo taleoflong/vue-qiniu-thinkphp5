386e9ac2c35952548525f42f684908674509d7b7
taleoflong <2530155570@qq.com>
2017-08-23 14:56:33 +0800

    Add files via upload

---
+++ Test.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="test">
+	<a class="btn" @click="toggleShow">上传图片</a>
+	<my-upload url="/upload"
+		lang-type="en"
+		field="img"
+		key="1"
+		max-size="500"
+		:only-single="true"
+		:value="true"
+		:only-img="true"
+		:params="params"></my-upload>
+</div>
+</template>
+
+<script>
+import mySwitch from 'vue-upload-file';
+import { Toast } from 'vant';
+export default {
+  name:'Test',
+  
+}
+</script>
+
+<style scoped>
+#test {
+		position: relative; /*控件上级容器必须是相对定位或绝对定位*/
+	}
+</style>
